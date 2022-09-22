bf3d99d75512c4f105943436aabe13603af6d452
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-22 23:23:41 +0300

    Show category buttons

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES}/>
       <NewTaskForm />
       <TaskList tasks={tasksToShow} setTasksToShow={setTasksToShow} />
     </div>
